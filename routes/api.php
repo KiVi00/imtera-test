a5dbf08cc050022ba78884f3e38201e70b529eea
Kirill <vinogradovkirill123@gmail.com>
2026-09-11 23:02:31 +0700

    move organization routes under auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,17 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrganizationController;
 
-Route::get('/ping', fn () => response()->json(['pong' => true]));
+Route::get('/ping', fn() => response()->json(['pong' => true]));
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/organizations', [OrganizationController::class, 'index']);
+    Route::post('/organizations', [OrganizationController::class, 'store']);
+    Route::get('/organizations/{organization}', [OrganizationController::class, 'show']);
+    Route::get('/organizations/{organization}/reviews', [OrganizationController::class, 'reviews']);
 });
 
-Route::get('/organizations', [OrganizationController::class, 'index']);
-Route::post('/organizations', [OrganizationController::class, 'store']);
 
-Route::get('/organizations/{organization}', [OrganizationController::class, 'show']);
-Route::get('/organizations/{organization}/reviews', [OrganizationController::class, 'reviews']);
