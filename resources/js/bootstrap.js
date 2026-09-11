ea61b0121e459d67101ae5a2d88f590f3030ab82
Kirill <vinogradovkirill123@gmail.com>
2026-09-11 23:43:28 +0700

    Add global 401 handler to redirect to login

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,3 +4,15 @@ window.axios = axios;
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response?.status === 401) {
+            if (window.location.pathname !== '/login') {
+                window.location.href = '/login';
+            }
+        }
+        return Promise.reject(error);
+    }
+);
